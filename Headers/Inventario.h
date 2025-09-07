5afa39aa63e2abc476b529ba83299c1238558018
OwenTP27 <27owentp@gmail.com>
2025-09-06 21:30:55 -0600

    Logica Inventario 50%

--- Headers/Inventario.h
+++ Headers/Inventario.h
@@ -1,4 +1,62 @@
+#include "Libro.h"
+
 #ifndef INVENTARIO
 #define INVENTARIO
+
+
+typedef struct Inventario {
+	Libro libro;
+	int cantidad;
+	struct Inventario *siguiente;
+} Inventario;
+
+Inventario* construirInventario(Libro* nuevoLibro, int cantidadLibro) {
+	Inventario *nuevoInventario = malloc(sizeof(Inventario));
+	nuevoInventario->libro = *nuevoLibro;
+	nuevoInventario->cantidad = cantidadLibro;
+	nuevoInventario->siguiente = NULL;
+	return nuevoInventario;
+
+}
+
+void agregarLibro(Inventario* inventario, Inventario* libro) {
+	inventario->siguiente = libro;
+}
+
+void mostrarInventario(Inventario* inventario) {
+	while (inventario != NULL) {
+		printf("\nTitulo: %s\nAutor: %s\nPrecio: %.2f\nCantidad: %d\nCodigo: %s\n",
+			inventario->libro.nombre,
+			inventario->libro.autor,
+			inventario->libro.precio,
+			inventario->cantidad,
+			inventario->libro.codigo);
+
+		inventario = inventario->siguiente;
+	}
+}
+
+void guardarInventarioEnArchivo(Inventario* inventario) {
+	FILE* archivo = fopen("Proyecto1-Lenguajes\\Data\\Inventario.txt", "w");
+	if (archivo == NULL) {
+		perror("Error al abrir el archivo");
+		return;
+	}
+
+	while (inventario != NULL) {
+		fprintf(archivo,
+			"Titulo: %s\nAutor: %s\nPrecio: %.2f\nCantidad: %d\nCodigo: %s\n\n",
+			inventario->libro.nombre,
+			inventario->libro.autor,
+			inventario->libro.precio,
+			inventario->cantidad,
+			inventario->libro.codigo);
+
+		inventario = inventario->siguiente;
+	}
+
+	fclose(archivo); // cerrar archivo al terminar
+}
+
 
 #endif // !"INVENTARIO"
